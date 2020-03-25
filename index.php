6be1a3c98a1aaa8302c2b44d3bc20b3a5666a9e3
arivorzlav <62639913+arivorzlav@users.noreply.github.com>
2020-03-25 14:50:47 +0700

    Website pertama
    Ini adalah index.php nya

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+	<title>RAKYAT 2.0</title>
+    <link rel="shortcut icon" href="mylogo.jpg">
+</head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="style.css">
+<script type="text/javascript">
+	alert("Hi, Selamat Datang Di Web Saya :)");
+</script>
+<style type="text/css"> 
+        h1	{
+			text-align: center;
+			background-color: blue;
+			color: white;
+			position: fixed;
+			width: 100%;
+			margin-top: 0px;
+			top: 0;
+            opacity: 1;
+		}
+        h2	{
+			text-align: center;
+			background-color: white;
+			color: white;
+			position: fixed;
+			width: 100%;
+			bottom: 0;
+            right: 0;
+            margin-bottom: 0px;
+            opacity: 1;
+            text-decoration-line: none;
+		}
+</style>
+<br>
+<br>
+<br>
+<br>
+<body style="font-family : sans-serif; color: white; background-color: black; padding-left: 10px; padding-right: 10px;">
+<hr>
+<a href="https://www.youtube.com/watch?v=P7N1_NelcKg">VIDEO</a>  | 
+<a href="https://www.youtube.com/watch?v=KTQq4X2WsZE">PRODUSEN</a>  | 
+<a href="https://www.tokopedia.com/bluem-1/vending-machine-blue-mart">PRODUK</a>  | 
+<a href="#wa">KONTAK</a>  | <input></input>  <a href="https://google.com">Search Here</a></b><br><hr item-width="1350px">
+<h1 style="color: white;"> Ver 2.0 Retail Business, Bundle It With Vending Machine <br>( Can You See It? It's The Next Generation Bussines? ) </h1><hr item-width="1350px">
+
+    <iframe width="1180" height="400" src="https://www.youtube.com/embed/kawciJonyIw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    
+		<p align="justify";>Bagaimana cara membantu para pebisnis pemula / bisnis kecil / UMKM dalam memenangkan persaingan bisnis di era Industri 4.0 saat ini? Jawaban paling masuk akal adalah dengan selalu merangsang mereka untuk terus mempelajari hal baru, terus memberikan edukasi sehingga mereka dapat melihat <i>opportunity</i> yang ada di pasar. Sejalan dengan berita yang ada <a href="https://news.detik.com/adv-nhl-detikcom/d-3219850/bisnis-vending-machine-lepas-landas-di-2018-bagian-1">disini</a>. Kadang para pebisnis hanya memerlukan sebuah <b><i>leverage</i></b> atau <b>daya ungkit</b> yang tepat untuk dapat meng-<i>scale up</i> bisnis mereka. Ya proses bisnis yang efektif dan efisien adalah jawaban dari pertanyaan "Mengapa bisnis saya tidak berkembang dan stuck disini?". Efisiensi alur bisnis adalah satu proses bisnis yang jarang dipahami oleh para pebisnis pemula. Bahkan berdasarkan riset bisnis terdahulu, dari 100 bisnis yang baru berdiri tak akan melewati waktu tahun pertamanya, dan hanya 1 bisnis yang dapat bertahan. Namun tentunya hal ini bukanlah satu hal yang menakutkan bagi pebisnis yang gigih dan tegar seperti anda bukan? Beberapa <a href="https://google.com">sumber</a> seperti Google sebenarnya dapat digunakan untuk mendapatkan cara bagaimana caranya meningkatkan efisiensi bisnis.</p>
+		<p align="justify">Saat ini perkembangan teknologi informasi telah memasuki tahap matang, dan kita berada di titik dimana pada saat kita memerlukan data atau suatu informasi, kita bisa mendapatkan dengan sangat-sangat mudah, asal kita tahu caranya. Dalam dunia usaha kita sepakat bahwa untuk mengambil keputusan bisnis yang tepat, yang tentunya akan berimbas pada angka omset serta profit yang bagus. Sebaliknya apabila seorang pengusaha kesulitan mengakses informasi bisnisnya sendiri, baik berupa jumlah produk, harga pengambilan, harga jual, angka penjualan serta profit bersihnya. Maka hampir dapat dipastikan dapat diambilnya keputusan yang tidak tepat. Lalu apakah kemajuan teknologi sudah dapat memberi jawaban atas masalah tersebut? Maka jawabanya adalah Ya. Benar kita sudah sangat mampu mendapatkan kemudahan-kemudahan dalam mengakses proses input dan output bisnis kita dari hulu ke hilir. Salah satu solusi dari otomatisasi proses bisnis adalah dengan memanfaatkan Vending Machine</p>
+		<p align="justify">Dan yang perlu dipahami oleh pebisnis pemula adalah, semakin simpel dan mudah proses bisnis yang dijalankan maka akan semakin maksimal hasil yang didapatkan, karena baik itu biaya tempat, gaji, listrik dan lain sebagainya akan kecil. Hal ini tentu berkebalikan dengan kepercayaan pebisnis pemula di luar sana yang percaya bahwa semakin besar modal dan rumit sistem bisnisnya makan hasil akan besar. Apakah benar demikian? Jawabnya adalah " Belum Tentu ". Ya, belum tentu memulai bisnis dengan modal yang mencapai ratusan juta dipastikan omsetnya akan mencapai ratusan juta juga, Belum tentu dengan tanpa menggunakan gudang/mesin-mesin pabrik yang besar akan membatasi kapasitas produksi bisnis kita.</p>
+		<p align="justify">Bila kita memperhatikan dari sisi market saat ini, pola konsumsi para konsumen telah berubah. Mereka semakin menyukai layanan bisnis yang praktis dan simpel. Semakin praktis maka akan semakin diminati. Dan harga bukan lagi menjadi tolak ukur utama dalam pengambilan keputusan konsumen. Dan lebih ke arah cara pelayanan serta ketersediaan produk/jasa tersebut. Salah satu jenis bisnis yang saat ini memiliki ciri-ciri simple dan masih jarang dilirik adalah bisnis vending machine</p>
+        <p align="justify">Memang dari produsen yang ada saat ini, mereka mematok harga mesin yang masih cukup mahal. Namun tidak menutup kemungkinan bila kita pelajari dan produksi sendiri, kita dapat menyediakan mesin Vending Machine dengan harga yang lebih terjangkau bagi masyarakat.</p>
+      <br>
+Mesin jual otomatis (Inggris:Vending machine) adalah mesin yang dapat mengeluarkan barang-
+barang seperti makanan ringan, minuman ringan seperti minuman soda, alkohol, rokok, tiket
+lotre, produk konsumen dan bahkan emas dan permata untuk pelanggan secara otomatis.
+Layaknya penjual asli, mesin ini akan mengeluarkan barang yang kita inginkan setelah kita
+membayarnya dengan cara memasukkan sejumlah koin maupun uang kertas.
+</p>
+<p align="justify">
+Pada tahun 80-an terciptanya mesin penjual otomatis yang menjual permen karet. mesin tersebut diberi nama ( Glico ). Pada tahun 1897 ketika Perusahaan Manufaktur Pulver menambahkan angka kecil di sekitar mesin, yang akan bergerak setiap kali seseorang membeli permen karet dari mesin mereka. Gagasan ini melahirkan mesin penjual otomatis jenis baru yang dikenal sebagai ( stimulator perdagangan) dan kelahiran mesin slot serta mesin pinball pertama berakar pada perangkat ini. Ide menambahkan permainan pada mesin ini adalah untuk menaikkan insentifitas pembeli.
+Pada Desember 1970 salah satu industri pembuat mesin penjual otomatis di Dallas, Texas, membuat mesin penjual otomatis yang dapat berbicara yang disebut dengan ( venda talker ). Ketika pembeli memasukkan koin, mesin kemudian mengatakan ( terima kasih ) dan dan suara itu disuarakan oleh comic Henny Youngman.
+</p>
+<p><b style="color: lightblue;">Jenis-Jenis Vending Machine :</b></p>
+<p><b style="color: lightblue;">Mesin penjual permen karet - </b>Setelah mesin tersebut dimasukkan koin sebesar yang ditentukan, maka mesin tersebut otomatis akan mengeluarkan permen karet warna-warni berbentuk bulat. Pada tahun 1888, Thomas Adams Gum Company memperkenalkan mesin penjual otomatis pertama ke Amerika Serikat. Mesin-mesin dipasang pada platform kereta bawah tanah yang terdapat di New York City dan menjual permen karet rasa <i>Tutti-Frutti</i>.</p>
+<p><b style="color: lightblue;">Mesin penjual koran - </b>Mesin ini ditemukan di U.S. Mesin penjual otomatis ini menjual koran untuk mempermudah warga U.S mendapatkan koran ketika mereka sedang berada dijalan.</p>
+<p><b style="color: lightblue;">Mesin penjual minuman ringan - </b>Mesin ini adalah mesin yang paling populer diseluruh dunia. Mesin ini merupakan mesin penjual minimuan kaleng seperti minuman bersoda, minuman energi, dan minuman isotonik.</p>
+<p><b style="color: lightblue;">Mesin penjual makanan - </b>Di Jepang terdapat mesin penjual ramen. Tak hanya ramen, tetapi terdapat juga mesin penjual sayuran di china. Sehingga para ibu rumah tangga tidak perlu repot pergi ke pasar, berkotor-kotoran, karena disebagian daerah tertentu telah diletakkan mesin penjual otomatis.</p>
+<p align="justify">Mesin-mesin ini berkembang terus sesuai jamannya, dan tidak hanya empat jenis mesin penjual otomatis, namun masih banyak lagi jenisnya termasuk mesin penjual otomatis yang menjual emas. Namun, diatas adalah mesin otomatis yang populer yang terdapat di banyak negara. Inovasi dianggap sebagai salah satu cara untuk meningkatkan kualitas hidup manusia. Berbagai inovasi laku keras diadopsi karena terbukti mampu memudahkan kehidupan sehari-hari. Namun nampaknya tidak demikian dengan mesin jual otomatis (vending machine) di Indonesia.Kehadirannya yang membawa kemudahan untuk membeli barang ternyata tak sedemikian bersambut. Berbeda dengan Jepang, di sana kehadiran vending machine (VM) berkembang begitu pesat dan membuatnya hadir di setiap sudut dan belokan di jalan. Barang yang dijual pun tak lagi sebatas makanan dan minuman ringan. Di negeri sakura itu, VM menyediakan buah dan sayuran segar hingga pakaian jadi. Salah satu pendiri perusahaan penyewaan dan operator VM di Indonesia, PT Jatari Boreas Sabha (Jaatari Vending) Lalu Athma Sasmita, menyatakan perkembangan VM di Insonesia masih dalam tahap bayi. Jumlah VM yang ada di Indonesia pun tertinggal jauh jika dibandingkan jumlah yang ada di Jepang maupun negara lainnya.</p>
+<p align="justify"><b style="color: lightblue;">Cara kerja -</b>
+Mesin penjual otomatis membutuhkan uang koin maupun uang kertas untuk proses kerjanya. Namun, yang paling populer adalah uang koin. pada bagian sudut tertentu, terdapat lubang untuk memasukan uang koin. Uang koin yang telah dimasukkan akan jatuh kedalam pengkait dan mesin didalamnya akan berjalan secara otomatis. Mesin tersebut berjalan sambil menjatuhkan barang yang pembeli pilih, dan terakhir pembeli tinggal mengambil dari balik pintu kecil yang telah disediakan.</p><br><hr><br>
+<p><b style="color: lightblue;">A. Sejarah Vending Machine</b></p>
+<p align="justify"><b>Sejarah - </b>Vending machine pertama diperkirakan telah diciptakan pada abad pertama oleh Hero, seorang matematikawan dan insinyur terkenal karena semangat inovatif dari Alexandria. Penemuan Hero adalah mesin air suci dengan pembayaran satu koin. Untuk mendapatkan air suci ini pembeli memasukkan koin ke dalam mesin, di mana air akan mendarat di atas nampan. Nampan ini terpasang pada tuas, yang membuka katup, melepaskan air dan memastikan bahwa pembeli tidak terlalu banyak mendapat air suci, nampan dirancang untuk bergerak dengan berat koin. Air mengalir terus-menerus hingga ujung koin jatuh, dan kemudian katup tertutup. Mesin-mesin penjual ini harus menunggu Era Industri sebelum mereka datang ke menonjol. Mesin penjual modern yang pertama dioperasikan koin diperkenalkan di London, Inggris pada tahun 1880-an awal, mesin penjual kartu pos. Mesin penjual otomatis pertama di Amerika Serikat dibangun pada tahun 1888 oleh Thomas Adams Gum Company, menjual permen karet pada platform kereta bawah tanah. Ide menambahkan permainan sederhana untuk mesin ini sebagai insentif lebih lanjut untuk membeli datang pada tahun 1897 ketika. Beberapa perusahaan manufaktur menambahkan angka kecil, yang akan bergerak di sekitar tiap kali ada orang membeli beberapa permen dari mesin mereka. Ide sederhana ini melahirkan jenis baru perangkat mekanik yang dikenal sebagai <b>pendorong perdagangan</b>. Awal kelahiran mesin slot dan <i>pinball</i> pada akhirnya berakar pada perangkat ini.</p>
+<p align="justify">Pada Desember 1970 Ussery Industri Dallas, Konvensi Texas di Dallas yang metampilkan vending machine yang terbaru ( dapat berbicara ), Venda. Dengan memasukkan koin, mesin akan mengatakan ( terima kasih ) dan menambahkan satu-liner ( Lelucon ) yang disuarakan oleh komika Henny Youngman. Venda adalah bagian pada Johnny Carson Tonight Show pada bulan Maret 1971.</p>
+<p align="justify">Jepang memiliki jumlah tertinggi vending mesin per kapita, dengan sekitar satu mesin untuk setiap 23 orang. kepadatan penduduk yang tinggi. Jepang, biaya tenaga kerja relatif tinggi, ruang terbatas, preferensi untuk berbelanja dengan berjalan kaki atau dengan sepeda, dan tingkat rendah vandalisme dan kejahatan kecil, menyediakan lingkungan yang subur bagi mesin penjual. Sementara sebagian besar mesin di Jepang dipenuhi dengan minuman, makanan ringan, dan rokok, terkadang menemukan mesin penjual menjual barang-barang seperti botol minuman keras, kaleng bir, makanan goreng, pakaian, ipod, lobster hidup, daging segar, telur dan tanaman pot. Praktek ( Vendo ), atau berdiri berlama-lama di sekitar mesin penjual otomatis untuk diminum dalam satu kelompok, tidak pernah terdengar selama bulan-bulan hangat. Mesin penjual otomatis pertama di Jepang terbuat dari kayu dan dijual perangko dan kartu pos. Sekitar 80 tahun yang lalu, ada mesin penjual yang menjual permen disebut ( Glico ). Pada tahun 1967, koin 100-yen distribusikan untuk pertama kalinya, dan penjualan mesin penjual otomatis meroket semalam, Menjual berbagai macam barang di mana-mana. Di Jepang, vending machine yang dikenal sebagai ( Jido - Hanbaiki ) dari Jido, atau ( otomatis ); Hanbai, atau ( penjual ), dan Ki, atau ( mesin ), ( Jihanki ) untuk pendek. Pada tahun 1999, diperkirakan 5,6 juta koin dan kartu-dioperasikan mesin penjual Jepang menghasilkan $ 53.280.000.000 dalam penjualan. mesin penjual barang dan jasa dapat biaya sesedikit 50 dan sebanyak Yen 3000. Ponsel sekarang dapat digunakan untuk membayar barang yang dibeli dari mesin penjual otomatis lebih mudah. Pada tahun 2008, kartu cerdas yang disebut <i>Taspo</i> diterapkan di sebagian besar mesin penjual tembakau di seluruh negeri untuk membatasi penjualan rokok dari mereka. Dari mesin seperti pembelian rokok hanya dapat dilakukan oleh orang-orang yang memiliki kartu, yang dikeluarkan untuk pelamar dewasa ( yang dalam kasus Jepang, adalah 20 tahun ). Kartu ini dimiliki hingga sensor setelah uang dimasukkan ke dalam mesin. Jenis umum snack bar di Belanda disebut automatiek dan mirip dengan sebuah otomatis. Ini memiliki dinding dilapisi dengan mesin yang dioperasikan koin. Masing-masing memiliki deretan vertikal jendela kecil, dengan makanan ringan (biasanya panas) di belakang masing-masing, misalnya sebuah kroket, dan sebuah hamburger. Setelah memasukkan koin ke dalam slot, seorang individu membuka salah satu jendela dan menghilangkan snack. Mesin-mesin dipanaskan sehingga jajanan tetap panas. Belakang mesin dapur di mana makanan ringan disusun, dengan jendela kecil yang kembali dipasok dari belakang. Selain itu, snack counter untuk makanan kurang cocok untuk mesin penjual biasanya tersedia di daerah yang sama (contoh: kentang goreng). Otomatis atau tidak menyediakan kursi untuk pelanggan. Kadang kadang mesin penjual berada dalam dinding luar, dan tidak ada tempat berteduh disediakan. Mesin penjual ini sering berada di stasiun kereta api atau di jalan-jalan sibuk.</p>
+<p><b style="color: lightblue;">C. Tujuan Vending Machine</b></p>
+<p align="justify">Tujuan dari vending machine adalah untuk memudahkan konsumen membeli minuman atau makanan ringan. Dengan begitu proses jual-beli dapat lebih mudah, efisiensi, praktis dan tidak menghabiskan waktu yang banyak merupakan pertimbangan utama manusia dalam melakukan aktivitas. Disamping itu wawasan dan ilmu pengetahuan kita terhadap perkembangan teknologi pun semakin bertambah, yaitu:</p>
+<p>1. Memodernisasi penjualan konvesional.</p>
+<p>2. Memudahkan pembeli untuk medapatkan makanan dan minuman kapan dan dimana saja. Tanpa takut toko akan tutup.</p>
+<p>3. Menghemat ruang.</p>
+<p>4. Mengurangi biaya jika menggunakan toko konvesional.</p><br>
+
+<p><b style="color: lightblue;">D. Manfaat Vending Machine</b></p>
+
+<p>Mesin penjual otomatis ( vending machine ) yang membuat kita nyaman bisa beli apapun tanpa harus repot, yaitu dengan:</p>
+<p>1. Memudahkan pelanggan dalam bertransaksi.</p>
+<p>2. Menghemat waktu.</p>
+<p>3. Lebih efisien dan efektif, karena biaya yang dikeluarkan lebih ringan.</p>
+
+<p><b style="color: lightblue;">E. Vending Machine di Indonesia</b></p>
+<p align="justify">Sekarang ini kita juga dapat menemukan vending machine di Indonesia, namun hanya di beberapa tempat saja kita dapat menjumpainya, tidak seperti di Jepang atau negara maju lainnya kita dapat menemukan vending machine dengan mudahnnya. Vending Machine ada di Indonesia umumnya hanya menjual minuman saja dan minuman yang dijual biasanya minuman sejenis softdrink dan dalam bentuk kaleng atau botol. Vending Machine  kebanyakan didatangkan atau diimport dari luar negeri, seperti Jepang, Korea, China, Amerika, dan lainnya. Sebenarnya di Indonesia ada perusahaan yang memproduksinya, yaitu PT.METEC SEMARANG yang berlokasi di kawasan berikat Tanjung Emas Export Procesing Zone, Semarang Jawa Tengah. Namun produk yang dihasilkan diexport lagi dan dipasarkan di Jepang, karena perusahaan ini merupakan anak cabang dari perusahaan KUBOTA, Japan yang memproduksi vending machine. Dan ada pula perusahaan yang bergerak di bidang perdagangan dan jasa khususnya pengadaan Vending Machine, yaitu perusahaan PT.PUTRI AQUARIUS yang didirikan pada tahun 29 maret 2011, dan berlokasi di pergudangan Sun Star Motor Blok A no.02 Komplek Surya Inti Permata, Raya Juanda Sidoarjo Jawa Timur. Visi dan misi perusahaan PT. PUTRI AQUARIUS adalah "Menjadi perusahaan yang unggul dan maju dalam bidang perdagangan dan jasa khususnya dalam bidang vending machine yang dibutuhkan dalam dunia pelayanan jasa di Indonesia serta mampu menghadapi persaingan global".</p>
+<p>Misi:</p>
+<p>1. Mengutamakan mutu dan pelayanan demi kepuasan pelanggan.</p>
+<p>2. Menjadi perusahaan yang andal dan terpecaya.</p>
+<p>3. Menciptakan peluang dan pertumbuhan untuk menjadi perusahaaan yang terbaik.</p>
+<p>4. Mengembangkan karyawan yang berkompeten.</p>
+<p>5. Menciptakan lingkungan kerja yang harmonis dan sejahtera.</p><br>
+
+<p><b style="color: lightblue;">F. Cara Penggunaan Vending Machine</b></p>
+<p align="justify">Cara penggunaan vending machine sangatlah mudah yaitu kita tinggal memasukan uang koin atau kertas (ada juga yang menggunakan kupon), lalu kita tekan tombol sesuai dengan barang yang kita inginkan, maka barang yang kita pilih akan dengan sendiri keluar, karena vending machine ini masih jarang dipakai di Indonesia, maka ada beberapa vending machine yang oleh pengelola vending machine ditempatkan orang untuk menjaga Cending Machine dan untuk melayani serta menjelaskan cara pembelian dengan vending machine. Tidak perlu takut dan khawatir untuk membeli melalui vending machine, karena  biasanya di lengkapi dengan petunjuk cara pembelian.
+</p>
+<img src="orang.jpg" width="800">
+	<iframe src="https://www.waroengrakjat.com/info" width="800" height="800" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+    <hr item-width="800">
+	<p><b><center style="color: grey;">Copyright 2019, PT Rakyat Berdagang Sejahtera.</center></b></p>
+	<p><b><center style="color: grey;">For more information call WA/TELEPON<div id="wa"></div> Admin 085647084526 (Arief) <a href="https://wa.me/6285647084526"> -->        klik untuk chat <--</a></center></b></p><hr><br><br>
+    <h2><a href="https://wa.me/6285647084526">SILAHKAN KLIK DISINI UNTUK INFO LEBIH LENGKAP - WA 085647084526 | email: ariefpriyambodo@gmail.com <br> Web Portofolio (Segala informasi yang terdapat di dalam website ini masih dalam tahap pengembangan tim).</a></h2>
+
+</body>
+</html>
